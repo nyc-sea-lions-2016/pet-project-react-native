452a88091d2b35c7cd0b3de3da0f8755664d66e5
FJ Collins Jr <fcollins@live.com>
2016-04-22 19:36:28 -0400

    Add navigator bar with links to profile and favorites

--- index.android.js
+++
@@ -1,5 +0,0 @@
-var React = require('react-native');
-var { AppRegistry } = React;
-var PetProject = require('./PetProject');
-
-AppRegistry.registerComponent('PetProject', () => PetProject);
--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,5 @@
  var React = require('react-native');
  var { AppRegistry } = React;
  var PetProject = require('./PetProject');
- var UsersShow = require('./UsersShow');
 
  AppRegistry.registerComponent('PetProject', () => PetProject);
- AppRegistry.registerComponent('UsersShow', () => UsersShow);
