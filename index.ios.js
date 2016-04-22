df768f76ff29a830dfdf8226ca38f9183008d199
FJ Collins Jr <fcollins@live.com>
2016-04-22 15:07:44 -0400

    add UsersShow var

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,7 @@
  var React = require('react-native');
  var { AppRegistry } = React;
  var PetProject = require('./PetProject');
+ var UsersShow = require('./UsersShow');
 
  AppRegistry.registerComponent('PetProject', () => PetProject);
+ AppRegistry.registerComponent('UsersShow', () => UsersShow);
