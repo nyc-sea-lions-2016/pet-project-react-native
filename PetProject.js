585ddb7d2ebe8529ff583032552d6c53cc831bdb
Alyssa <aransbury@gmail.com>
2016-04-22 12:05:11 -0400

    Link rails api with homepage

--- PetProject.js
+++ PetProject.js
@@ -2,23 +2,72 @@ var React = require('react-native');
 
 var {
   StyleSheet,
+  Component,
   Text,
   View,
+  ListView,
   TextInput,
   Image
 } = React;
 
-var PetProject = React.createClass({
+var REQUEST_URL = 'http://localhost:3000/index.json';
 
-  render() { return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>
-        Welcome to this app.
-      </Text>
-    </View>
+class PetProject extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 != row2
+      }),
+      loaded: false,
+    }
+  }
+  componentDidMount(){
+    this.fetchData();
+  }
+  fetchData(){
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData)
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData),
+          loaded: true,
+        });
+      })
+      .done();
+  }
+  render() {
+    if (!this.state.loaded){
+      return this.renderLoadingView();
+    }
+
+    return (
+    <ListView
+      dataSource={this.state.dataSource}
+      renderRow={this.renderPet}
+      />
     );
   }
-});
+  renderLoadingView(){
+    return (
+      <View>
+        <Text>
+          Loading pets...
+        </Text>
+      </View>
+    )
+  }
+  renderPet(pet){
+    return (
+      <View>
+        <Text>
+          HERE IS WHERE THE PET IS
+        </Text>
+      </View>
+    )
+  }
+};
 
 var styles = StyleSheet.create({
   container: {
